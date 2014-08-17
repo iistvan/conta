2a161471a9275b311119714000f5f7b352a17568
attilabajko <attilabajko@yahoo.com>
2014-08-17 11:49:19 +0300

    a

---
+++ Programocska/Programocska/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿using MySql.Data.MySqlClient;
+/*
+ * Created by SharpDevelop.
+ * User: Shiva
+ * Date: 16.08.2014
+ * Time: 21:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Programocska
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGrid1 = new System.Windows.Forms.DataGrid();
+			this.dataSet1 = new System.Data.DataSet();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGrid1
+			// 
+			this.dataGrid1.AlternatingBackColor = System.Drawing.Color.Lavender;
+			this.dataGrid1.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.dataGrid1.BackgroundColor = System.Drawing.Color.LightGray;
+			this.dataGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.dataGrid1.CaptionBackColor = System.Drawing.Color.LightSteelBlue;
+			this.dataGrid1.CaptionFont = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.dataGrid1.CaptionForeColor = System.Drawing.Color.MidnightBlue;
+			this.dataGrid1.DataMember = "";
+			this.dataGrid1.DataSource = this.dataSet1;
+			this.dataGrid1.FlatMode = true;
+			this.dataGrid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.dataGrid1.ForeColor = System.Drawing.Color.MidnightBlue;
+			this.dataGrid1.GridLineColor = System.Drawing.Color.Gainsboro;
+			this.dataGrid1.GridLineStyle = System.Windows.Forms.DataGridLineStyle.None;
+			this.dataGrid1.HeaderBackColor = System.Drawing.Color.MidnightBlue;
+			this.dataGrid1.HeaderFont = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.dataGrid1.HeaderForeColor = System.Drawing.Color.WhiteSmoke;
+			this.dataGrid1.LinkColor = System.Drawing.Color.Teal;
+			this.dataGrid1.Location = new System.Drawing.Point(-1, 10);
+			this.dataGrid1.Margin = new System.Windows.Forms.Padding(4);
+			this.dataGrid1.Name = "dataGrid1";
+			this.dataGrid1.ParentRowsBackColor = System.Drawing.Color.Gainsboro;
+			this.dataGrid1.ParentRowsForeColor = System.Drawing.Color.MidnightBlue;
+			this.dataGrid1.SelectionBackColor = System.Drawing.Color.CadetBlue;
+			this.dataGrid1.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+			this.dataGrid1.Size = new System.Drawing.Size(357, 183);
+			this.dataGrid1.TabIndex = 0;
+			this.dataGrid1.UseWaitCursor = true;
+			this.dataGrid1.CurrentCellChanged += new System.EventHandler(this.DataGrid1CurrentCellChanged);
+			// 
+			// dataSet1
+			// 
+			this.dataSet1.DataSetName = "NewDataSet";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(271, 200);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 28);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Iesire";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.UseWaitCursor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(358, 240);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGrid1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Location = new System.Drawing.Point(50, 30);
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Programocska";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Data.DataSet dataSet1;
+		private System.Windows.Forms.DataGrid dataGrid1;
+		
+		void DataGrid1CurrentCellChanged(object sender, System.EventArgs e)
+		{
+			//MainForm.szineszek.Update(this.dataSet1);
+		}
+		
+		void Button1Click(object sender, System.EventArgs e)
+		{
+			
+		}
+	}
+}
---
+++ Programocska/Programocska/MainForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Shiva
+ * Date: 16.08.2014
+ * Time: 21:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace Programocska
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//kapcsolodas az adatbazishoz
+			 MySqlConnection kapcsolat = new MySqlConnection("SERVER=85.10.205.173;DATABASE=ganesha;UID=attilabajko;PASSWORD=shivashakti");
+			 string queryString = "SELECT * FROM Utilizatori";
+			 MySqlCommand parancs = new MySqlCommand(queryString, kapcsolat);
+        	 MySqlDataAdapter users = new MySqlDataAdapter(queryString,kapcsolat);
+			 kapcsolat.Open();
+			 users.Fill(this.dataSet1);
+			 this.dataGrid1.DataSource = this.dataSet1;	
+			 this.dataGrid1.DataMember = this.dataSet1.Tables[0].TableName;
+			 users.Update(this.dataSet1);
+			 kapcsolat.Close();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
